a2ada825c853d9e74e95658133b2322c99dbcdb9
Julian Enochs <julianenochs@yahoo.com>
2020-02-01 11:06:31 -0700

    Update styling

--- src/components/NewsContainer/NewsContainer.test.js
+++ src/components/NewsContainer/NewsContainer.test.js
@@ -4,22 +4,13 @@ import NewsContainer from './NewsContainer';
 
 describe('News Container', () => {
     it('should match the snapshot with all data passed in correctly', () => {
-<<<<<<< HEAD
-        const wrapper = shallow(<NewsContainer 
-=======
         const wrapper = shallow(<NewsContainer
->>>>>>> Search
             key={69}
             headline='Party on Garth'
             img='someURL'
             description='Party on Wayne'
             url='someURL'
-<<<<<<< HEAD
-            />);
-            expect(wrapper).toMatchSnapshot();
-=======
         />);
         expect(wrapper).toMatchSnapshot();
->>>>>>> Search
     });
 });
